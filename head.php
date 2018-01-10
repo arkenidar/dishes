77dd70e15b8bc5300ec2a726c3b44910e4ebb907
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-01-10 19:35:25 +0100

    active link

--- head.php
+++ head.php
@@ -1,2 +1,16 @@
 <h1>Tour of Dishes</h1>
-<a href="dashboard.php">Dashboard</a> <a href="dishes.php">Dishes</a>
+<a href="dashboard.php" class="nav">Dashboard</a> 
+<a href="dishes.php" class="nav">Dishes</a>
+
+<!-- active link -->
+<style>
+.nav { text-decoration: none; }
+.active { text-decoration: none; border: 1px solid red; }
+</style>
+<script>
+document.querySelectorAll('.nav').forEach(
+    function(link){
+        if(link.href==location.href)link.className='active';
+    }
+)
+</script>
